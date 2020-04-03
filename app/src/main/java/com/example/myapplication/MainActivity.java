5ec7c73e8d5307a10da896e9032153f43c2e4d2b
Meriliana <cmeriliana@yahoo.com>
2020-04-03 14:15:49 +0800

    comit6

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.choose_recognation);
+        setContentView(R.layout.identification_result_activity);
     }
 }
